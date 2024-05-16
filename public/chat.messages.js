0549b28ec477df98c212cc7b7d7279e838c86602
kami-note <yurinote666@gmail.com>
2024-05-16 13:50:31 -0300

    main

--- public/chat.messages.js
+++ public/chat.messages.js
@@ -3,18 +3,22 @@ const fs = require('fs');
 const jsonString = fs.readFileSync('public/messages.json', 'utf-8');
 const jsonMessages = JSON.parse(jsonString);
 
+function formatMessage(message) {
+    return message.replace(/[^\w]/gi, '').trim().toLowerCase();
+}
+
 function initMessages(client) {
     client.on('message_create', receivedMessage => {
         if (receivedMessage != null) {
-            switch (receivedMessage.body) {
-                case 'ping':
-                    client.sendMessage(receivedMessage.from, 'pong');
-                    break;
-                case 'Bom dia':
-                    client.sendMessage(receivedMessage.from, jsonMessages.wellcome);
-                    break;
-                default:
-                    break;
+            const receivedBody = formatMessage(receivedMessage.body);
+            console.log('Received message:', receivedBody);
+            const response = jsonMessages[receivedBody];
+            if (response) {
+                console.log('Sending response:', response);
+                client.sendMessage(receivedMessage.from, response);
+            } else if (response === null){
+                console.log('No response found for:', receivedBody);
+                client.sendMessage(receivedMessage.from, "Desculpe, não entendi a sua pergunta.");
             }
         }
     });
